5e2d60c8751f232673beb61e8f736646c0a4f2d8
Safi100 <safinafi12@gmail.com>
2025-03-10 17:42:03 +0200

    new employee page

---
+++ src/pages/newEmployee/NewEmployee.jsx
@@ -0,0 +1,97 @@
+import { useState } from "react";
+import axios from "axios";
+import { ToastContainer } from "react-toastify";
+import { Notify } from "../../components/Notify";
+import "./newEmployee.css";
+
+const NewEmployee = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    phone: "",
+    email: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    axios
+      .post("http://localhost:8000/api/admin", formData)
+      .then((res) => {
+        Notify(res.data.message);
+        setFormData({ name: "", phone: "", email: "" });
+      })
+      .catch((error) => {
+        console.error("Error response:", error.response);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  return (
+    <div className="wrapper py-4">
+      <ToastContainer />
+      <h2 className="mb-4 text-center">Add New Admin</h2>
+      <form className="new_employee_form" onSubmit={handleSubmit}>
+        <div className="row">
+          <div className="col-md-6 mb-3">
+            <label className="form-label">Full Name</label>
+            <input
+              type="text"
+              name="name"
+              className={`form-control`}
+              placeholder="Enter full name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className="col-md-6 mb-3">
+            <label className="form-label">Email</label>
+            <input
+              type="email"
+              name="email"
+              className={`form-control`}
+              placeholder="Enter email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className="col-md-6 mb-3">
+            <label className="form-label">Phone</label>
+            <input
+              type="text"
+              name="phone"
+              className={`form-control`}
+              placeholder="Enter phone number"
+              value={formData.phone}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className="col-12">
+            <button
+              type="submit"
+              disabled={loading}
+              className="btn btn-primary mt-3"
+            >
+              {loading ? "Adding..." : "Add Employee"}
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default NewEmployee;
